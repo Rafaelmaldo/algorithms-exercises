a65bb3ad2a3720a8be32ac2cbd4fe4da495c2e59
Rafael Maldonado <rafaelmaldo387@gmail.com>
2024-09-10 12:52:01 -0400

    moving to this forked repo from learning

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,27 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
-};
+  const merge = (sortedArray, mergingArray) => {
+    for(num of mergingArray){
+      let j;
+      for (j = sortedArray.length - 1; num < sortedArray[j] && j >= 0; j--) {
+          sortedArray[j + 1] = sortedArray[j];
+      }
+      sortedArray[j + 1] = num;
+    }
+    return sortedArray;
+  }
+  
+  if (nums.length === 1) {
+    return nums;
+  }
+
+  return merge(mergeSort(nums.slice(0, Math.floor(nums.length / 2))), mergeSort(nums.slice(Math.floor(nums.length / 2))))
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
