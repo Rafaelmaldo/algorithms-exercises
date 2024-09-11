dae7e52840b5f6e84d4659bc8a3a06fb8c78b893
Rafael Maldonado <rafaelmaldo387@gmail.com>
2024-09-11 17:54:08 -0400

    update radix sort in order to pass test

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -1,28 +1,23 @@
-/*
-
-  Implement a radix sort in a function called radixSort.
-
-  You'll probably need several functions
-  
-  You can implement it using a binary or decimal based bucketing but I'd recommend the decimal based buckets because
-  it ends up being a lot more simple to implement.
-
-*/
-
 const getDigit = (number, place, _longestNumber) => {
+
+  // check if the number has enough digits to be sorted
+  // for example number = 1, place = 4 would break otherwise
   if ((number.toString().length <= place)) {
     return 0;
   }
 
+  // make number an array and pop off the last element
+  // then get the number at the current place 
+  // for example (number = 3451, place = 2) -> 4
   const numberAsArray = (number.toString()).split("");
   const digit = Number(numberAsArray[(numberAsArray.length - 1) - place]);
   return digit;
 }
 
-
 const getLongestNumber = (array) => {
   let highestDigits = 1;
   
+  // loop through the array and keep track of the amount of digits and update if higher
   for (num of array) {
     let digits = num.toString().length;
     if (digits > highestDigits) {
@@ -30,32 +25,40 @@ const getLongestNumber = (array) => {
     }
   }
 
+  // return the digits of the highest value
+  // for example [2345] -> 4
   return highestDigits;
 }
 
 const radixSort = (array) => {
-  if (array.lenth < 2) {
-    return array;
-  }
-
+  // Find number with the most digits
   const longestNumber = getLongestNumber(array);
+
+  // Loop once for each digit
   for (let i = 0; i < longestNumber; i++) {
+
+    // Create buckets to queue the numbers into
     const buckets = []
     for (let j = 0; j <= 9; j++){
       buckets.push([]);
     }
 
+    // while there are still elements in the array
     while (array.length) {
+
+      // remove the first element and get the digit we are on (i) and add to corresponding bucket
       let addToBucket = array.shift();
       let bucketNumber = getDigit(addToBucket, i);
       buckets[bucketNumber].push(addToBucket);
     }
 
+    // remove the elements from the buckets and add them to the front of the array
     for (let bucket of buckets) {
-      array = [...array, ...bucket]
+      array.push(...bucket)
     }
-
   }
+
+  // return sorted array
   return array;
 }
 
--- src/Sort.js
+++ src/Sort.js
@@ -5,11 +5,66 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 import "./sort.css";
 
 function sort(array) {
-  // do cool stuff here
+  const getDigit = (number, place, _longestNumber) => {
 
-  // call snapshot any time you do anything to the array
-  // it's okay if you call it with duplicate value array,
-  // it will deduplicate for you
+    // check if the number has enough digits to be sorted
+    // for example number = 1, place = 4 would break otherwise
+    if ((number.toString().length <= place)) {
+      return 0;
+    }
+  
+    // make number an array and pop off the last element
+    // then get the number at the current place 
+    // for example (number = 3451, place = 2) -> 4
+    const numberAsArray = (number.toString()).split("");
+    const digit = Number(numberAsArray[(numberAsArray.length - 1) - place]);
+    return digit;
+  }
+  
+  const getLongestNumber = (array) => {
+    let highestDigits = 1;
+    
+    // loop through the array and keep track of the amount of digits and update if higher
+    for (num of array) {
+      let digits = num.toString().length;
+      if (digits > highestDigits) {
+        highestDigits = digits;
+      }
+    }
+  
+    // return the digits of the highest value
+    // for example [2345] -> 4
+    return highestDigits;
+  }
+  
+  // Find number with the most digits
+  const longestNumber = getLongestNumber(array);
+
+  // Loop once for each digit
+  for (let i = 0; i < longestNumber; i++) {
+    snapshot(array);
+    // Create buckets to queue the numbers into
+    const buckets = []
+    for (let j = 0; j <= 9; j++){
+      buckets.push([]);
+    }
+
+    // while there are still elements in the array
+    while (array.length) {
+
+      // remove the first element and get the digit we are on (i) and add to corresponding bucket
+      let addToBucket = array.shift();
+      let bucketNumber = getDigit(addToBucket, i);
+      buckets[bucketNumber].push(addToBucket);
+    }
+
+    // remove the elements from the buckets and add them to the front of the array
+    for (let bucket of buckets) {
+      array.push(...bucket)
+    }
+    snapshot(array);
+  }
+
   snapshot(array);
 }
 
